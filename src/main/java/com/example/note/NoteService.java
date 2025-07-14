7ee8894e034eb92193412c30006cf6500a6b25d9
Max Sikorsky <pain.maker28@gmail.com>
2025-07-14 16:55:34 +0700

    commit test

--- src/main/java/com/example/note/NoteService.java
+++ src/main/java/com/example/note/NoteService.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.Optional;
 
 /**
- * Класс представляет методы для управлением заметками.
+ * Класс представляет методы для управлением заметкой.
  */
 public class NoteService {
   private final Map<Integer, Note> notes = new HashMap<>();
